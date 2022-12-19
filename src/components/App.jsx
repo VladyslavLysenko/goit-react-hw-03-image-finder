a913f2e3df99cf7ec609535cba21234427af26ca
VladyslavLysenko <Dodog12362@gmail.com>
2022-12-19 21:20:47 +0200

    summary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { SearchBar } from './SearchBar/SearchBar';
+import { Toaster } from 'react-hot-toast';
+
+export class App extends Component {
+  state = {
+    photoName: null,
+  };
+
+  // handleFormSubmit = pokemonName => {
+  //   this.setState({ pokemonName });
+  // };
+
+  componentDidMount() {
+    fetch(
+      `https://pixabay.com/api/?q=cat&page=1&key=30662426-21982097d0559eebc608a0eec&image_type=photo&orientation=horizontal&per_page=12`
+    )
+      .then(res => res.json())
+      .then(photo => this.setState({ photo }));
+  }
+
+  handleFormSubmit = photoName => {
+    this.setState({ photoName });
+    console.log(photoName);
+  };
+
+  render() {
+    return (
+      <div style={{ maxWidth: 1170, margin: '0 auto', padding: 20 }}>
+        <Toaster/>
+        <SearchBar onSubmit={this.handleFormSubmit} />
+      </div>
+    );
+  }
+}
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,44 @@
+import { Component } from 'react';
+import toast from 'react-hot-toast';
+// Реакт тост 29 хв
+export class SearchBar extends Component {
+  state = {
+    photoName: '',
+  };
+
+  handleNameChange = event => {
+    this.setState({ photoName: event.currentTarget.value.toLowerCase() });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+      if (this.state.photoName.trim() === '') {
+        toast('Уведіть назву для пошуку фото...');
+      return;
+    }
+    this.props.onSubmit(this.state.photoName);
+    this.setState({ photoName: '' });
+  };
+
+  render() {
+    return (
+      <header className="searchbar">
+        <form onSubmit={this.handleSubmit} className="form">
+          <button type="submit" className="button">
+            <span className="button-label">Search</span>
+          </button>
+
+          <input
+            onChange={this.handleNameChange}
+            className="input"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
+}
